5f323aab486b9477629953566bd5d530036b84ca
guilhermewt <welliston10@gmail.com>
2022-11-24 20:42:22 -0300

    adicionado header, concertado bug que nao rodava o npm start

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataTablesModule } from "angular-datatables";
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './component/template/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DataTablesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
